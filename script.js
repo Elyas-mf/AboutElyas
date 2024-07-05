19900fb8d21d5e5fcafd9438693e37d388a4f672
elyas <elyasmohammed78@gmail.com>
2024-07-05 04:42:44 +0300

    php2

--- script.js
+++ script.js
@@ -107,3 +107,24 @@ const typedd = new Typed('.multiple-text-about', {
 
 });
 
+
+
+function submitForm(event) {
+  event.preventDefault(); // Prevent the default form submission
+
+  const form = document.getElementById('contactForm');
+  const formData = new FormData(form);
+
+  fetch('http://localhost/project/receiveForm.php', {
+      method: 'POST',
+      body: formData,
+  })
+  .then(response => response.text())
+  .then(data => {
+      document.getElementById('response').innerHTML = '<p>Thank you for your message!</p>';
+      form.reset(); // Reset the form fields
+  })
+  .catch(error => {
+      console.error('Error:', error);
+  });
+}
